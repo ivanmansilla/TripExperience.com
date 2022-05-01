ecfdd62d7dcae0c64e6ed3a9ca2e1675d828f7db
martialtimira <zignar1999@gmail.com>
2022-05-01 12:55:44 +0200

    routerLink to help button

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -3,7 +3,9 @@
     <router-link to="/"
       ><img class="logo" src="../assets/Triplogo.png"
     /></router-link>
-    <li class="headerItem"><a href="https://www.google.com"> Ayuda </a></li>
+    <router-link to="/about"
+      ><li class="headerItem"><a> Ayuda </a></li></router-link
+    >
     <li class="headerItem"><a href="https://www.google.com"> Idioma </a></li>
     <li class="headerItem">
       <a href="https://www.google.com"> Iniciar Sesion </a>
