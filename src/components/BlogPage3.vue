0f7e6ad24070fde2e2efeaaed5e634b152817281
ivanmansilla <ivanmansillaflores@gmail.com>
2022-05-05 16:17:09 +0100

    Blog Post Usuarios

---
+++ src/components/BlogPage3.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="container-card">
+    <div class="row">
+      <div class="text-column" style="text-align: left">
+        <h3 class="title">POST USUARIO</h3>
+        <body>
+          <div class="column">
+            <img
+              class="profile-picture"
+              src="../assets/FerranFerrero.jpg"
+              align="right"
+            />
+          </div>
+        </body>
+        <br />
+        <div class="column" style="text-align: centre">
+          <br /><br />
+          FERRAN FERRERO
+          <br /><br />
+          38 AÑOS, VALENCIA
+          <br /><br />
+          Experiencia con paracaidas
+        </div>
+        <div class="column" style="text-align: left">
+          <br />
+          <h3>Fotos de la experiencia realizada:</h3>
+          <img class="photo-column" src="../assets/para1.jpg" align="left" />
+          <br />
+          <br />
+          <img class="photo-column" src="../assets/para2.jpg" align="left" />
+          <br />
+          <br />
+          <img class="photo-column" src="../assets/para3.jpg" align="left" />
+          <br />
+          <br />
+          <img class="photo-column" src="../assets/para4.jpg" align="left" />
+          <br />
+          <br />
+          <img class="photo-column" src="../assets/para5.jpg" align="left" />
+        </div>
+        <br />
+      </div>
+    </div>
+    <div class="text-blog">
+      <h2>PARACAIDAS, UNA EXPERIENCIA INOLVIDABLE</h2>
+      <p>
+        Quería contaros cómo fue mi experiencia por si alguno estáis en la misma
+        situación que yo. Saltar en paracaidas es una de esas cosas que siempre
+        he tenido en mente. Siempre ha estado en la lista de los sueños de cosas
+        que quiero hacer algún día, como viajar a la india a estar un mes
+        inmersa en el mundo del Yoga o perder el miedo a bucear. La mayoría de
+        veces, fantaseamos con este tipo de cosas, pero nos cuesta tomar la
+        determinación de elegir una fecha, concretar el sitio y hacerlas
+        realidad.<br /><br />El problema de las cosas que queremos hacer algún
+        día, es que es muy fácil caer en el clásico “ya lo hare, ya lo haré” y
+        al final no lleguen nunca. Porque la vida pasa demasiado deprisa, y
+        muchas veces nos cuesta cortar con la rutina y decir, ahora es el
+        momento. Ahora voy a saltar. Ya sea en paracaidas o cualquiera que sea
+        tu sueño. Y así era para mí, el salto que algún día haría hasta que
+        recibí una llamada un jueves por lanoche y me dijo: Ferran, el sábado
+        por la mañana nos vamos a tiranos en paracaídas, yo lo voy a hacer,
+        tengo reserva a través de TripExperience, así que tú decides si quieres
+        hacerlo”.<br />La verdad es que no lo dudé un instante, decidí que iba a
+        hacerlo, pero sobre todo, tomé la mejor decisión que podía, y es que iba
+        a disfrutar cien por cien la experiencia. No iba a dejar que el miedo o
+        los nervios pudieran conmigo o emborronaran mi recuerdo de ese día. Iba
+        a vivirlo con toda la serenidad que podía para poder disfrutar de cada
+        instante.<br />El sábado por la mañana me levanté a las 5 de la mañana
+        con la misma sensación de cuando tenía 8 años y tenía excursión del
+        colegio. Esa emoción que te hace salir de un salto de la cama con la
+        sonrisa puesta. Preparé mi cd favorito y me bajé al coche que tenía casi
+        dos horas de camino hasta el sitio. Canté, vi salir el sol ,
+        absolutamente sola en la carretera e imaginé mil veces cómo iba a ser
+        ese salto.<br /><br />Llegue a Skydive y salude a todos los
+        participantes, charlé con ellos despejando sus dudas y sus nervios,
+        conocí a los instructores que no me podían dar más confianza. Son
+        majísimos y realmente muy buenos instructores. Escuché con atención
+        todas las indicaciones que daban para realizar el salto correctamente y
+        con seguridad y me puse el traje con la sensación de que todo estaba más
+        que bien.<br />Tocaba subir a la avioneta, a mi lado, la particpante más
+        mayor de todos, Paloma, con 60 años de edad y enfundada en el mismo
+        traje amarillo que yo... No podía tener miedo, ella iba a saltar.
+        Empezamos a subir, y durante unos 15 minutos empiezas a ver como todo
+        allá abajo empieza a ser muy pequeñito. Pensé en la relatividad de las
+        cosas, de los problemas, de lo importante que es a veces coger
+        perspectiva para ver realmente la dimensión de cosas que a veces nos
+        parecen muy grandes, y en realidad no lo son. Me sorprendí a mí mismo
+        tan tranquilo, reflexionando sobre la metáfora que tenía todo aquello
+        con la vida. Me hubiera gustado comprobar esa sensación con mis
+        pulsaciones...jajaja.<br /><br />Llegaba el momento de saltar en
+        paracaídas, sabía que tenía que inclinar mi cuerpo hacia atrás y apoyar
+        la cabeza en el instructor para que el pudiera hacer las maniobras
+        libremente. En ese momento sólo pensaba en eso. No mire al suelo y no
+        tuve miedo, confiaba en él, y confiaba en que todo iba salir bien. Y así
+        sin apenas darme cuenta, saltamos del avión desde 4000 metros y empezó
+        la caída libre.<br /><br />Y esta es la parte más difícil de describir,
+        la sensación de caer a 200 km/h se apodera de todo tu cuerpo, no se
+        parece a nada, no es como cuando tienes vértigo y lo sientes en la
+        tripa, o nervios y lo sientes en el pecho. Es como una avalancha que
+        sensaciones que te hacen abrir la boca sin poder gritar por la velocidad
+        a la que caes, pero que a la vez detiene el tiempo y cualquier
+        pensamiento. No puedes más que flipar con lo que estás viviendo, no hay
+        miedo, sólo vida, 50 segundos de pura vida. Es increíble el manejo que
+        tienen estos especialistas de la situación, la chica que me hacía el
+        vídeo era capaz de ponerse justo debajo de mí y mantenerse ahí a esa
+        velocidad, cogerme la mano, chocar conmigo...alucinante. No eres capaz
+        de procesar a la velocidad que vas hasta que no se abre el paracaidas, y
+        del cambio de velocidad tienes la sensación de ir hacia arriba, cuando
+        tan sólo está descendiendo tu velocidad. Ese momento creo que es el más
+        impactante.<br /><br />El descenso con el paracaidas ya abierto se hace
+        hasta relajante, ya empiezas a distinguir las cosas en el suelo y tomar
+        perspectiva de la altura, cosa que durante la caída libre no pasa. Deben
+        ser unos dos minutos y mientras vas hablando con el monitor y haciendo
+        prácticas de lo que tienes que hacer para aterrizar. Al saltar en
+        paracaidas con el monitor tú no haces ninguna maniobra, tan sólo tienes
+        que levantar bien alto las piernas para le permitas a él llevar a cabo
+        el aterrizaje correctamente y tus pies no choquen contra el suelo. Yo
+        levanté tanto las piernas que me llevé un pequeño culetazo, peor os
+        aseguro que en ese momento no sentía ni padecía.<br /><br />La sensación
+        de después es como la de estar en una nube, y su efecto dura unos
+        cuantos días. Esa maravillosa sensación de que todo es posible, de que
+        nada es tan importante y de que la vida, es la leche!!! Os dejo por aquí
+        algunas fotos para que podáis vivir la experiencia conmigo, aviso que
+        sólo verlas ya os dan ganas de querer saltar en paracaídas. <br />Espero
+        que os guste amigos de TripExperience!
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "BlogView3",
+};
+</script>
+<style scoped>
+.container-card {
+  height: auto;
+  justify-content: first baseline;
+  display: flex;
+  border: 1px solid lightgray;
+  box-shadow: 2px 2px 8px 4px #d3d3d3d1;
+  border-radius: 15px;
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #fffcb7;
+}
+.text-column {
+  padding-left: 30px;
+  float: centre;
+  text-align: centre;
+}
+.text-top {
+  padding-top: 100px;
+  padding-right: 05px;
+  text-align: left;
+}
+.text-blog {
+  width: 50%;
+  margin: 20px auto;
+  color: #fff;
+  padding: 15px;
+}
+.column {
+  float: left;
+  padding-left: 20px;
+}
+.photo-column {
+  float: left;
+  padding-left: 20px;
+  width: 300px;
+  height: 200px;
+}
+.profile-picture {
+  padding-top: 20px;
+  border-radius: 10px;
+  float: left;
+  margin-left: 10px;
+  width: 200px;
+  height: 200px;
+}
+#inputDestination {
+  width: 300px;
+  height: 30px;
+}
+
+#destinationSearchBtn {
+  height: 36px;
+  background-color: orange;
+  color: white;
+  font-weight: bold;
+  border-color: white;
+  border-width: 3px;
+}
+</style>
---
+++ src/views/BlogView2.vue
@@ -0,0 +1,17 @@
+<template>
+  <BlogPage2 />
+  <br />
+</template>
+
+<script>
+import BlogPage2 from "@/components/BlogPage2.vue";
+
+export default {
+  name: "BlogView2",
+  components: {
+    BlogPage2,
+  },
+};
+</script>
+
+<style></style>
